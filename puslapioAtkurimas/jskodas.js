2af58987bfe408a4c8abc9dfdff0e1286efe3624
Aivaras <aivarasbp@gmail.com>
2024-03-05 22:45:43 +0200

    nu kiek galima

--- puslapioAtkurimas/jskodas.js
+++ puslapioAtkurimas/jskodas.js
@@ -11,3 +11,10 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+
+const parallax = document.getElementById("parallax");
+
+window.addEventListener("scroll", function () {
+  let offset = window.pageYOffset;
+  parallax.style.backgroundPositionY = offset * 0.7 + "px";
+})
