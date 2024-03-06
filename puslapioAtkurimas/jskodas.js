82fa53fb5e048d0826c232140a5070b9fb9535f4
Aivaras <aivarasbp@gmail.com>
2024-03-06 12:59:04 +0200

    update

--- puslapioAtkurimas/jskodas.js
+++ puslapioAtkurimas/jskodas.js
@@ -14,7 +14,23 @@ document.addEventListener("DOMContentLoaded", function () {
 
 const parallax = document.getElementById("parallax");
 
-window.addEventListener("scroll", function () {
-  let offset = window.pageYOffset;
-  parallax.style.backgroundPositionY = offset * 0.7 + "px";
-})
+// window.addEventListener("scroll", function () {
+//   let offset = window.pageYOffset;
+//   parallax.style.backgroundPositionY = offset * 0.7 + "px";
+// })
+// window.onscroll = function () { myFunction() };
+
+// // Get the header
+// var header = document.getElementById("myHeader");
+
+// // Get the offset position of the navbar
+// var sticky = header.offsetTop;
+
+// // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+// function myFunction() {
+//   if (window.pageYOffset > sticky) {
+//     header.classList.add("sticky");
+//   } else {
+//     header.classList.remove("sticky");
+//   }
+// }
