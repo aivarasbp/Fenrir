c6cda8c03ec8586cb1eb50c82004122851e3de37
Aivaras <aivarasbp@gmail.com>
2024-03-12 19:20:49 +0200

    009

---
+++ 009/app.js
@@ -0,0 +1,120 @@
+// console.log('hello world');
+
+// // OR ||
+
+// console.log(true || true);
+// console.log(true || false);
+// console.log(false || true);
+// console.log(false || false);
+
+
+// // AND &&
+
+// console.log(true && true);
+// console.log(true && false);
+// console.log(false && true);
+// console.log(false && false);
+
+// // NOT!
+
+// console.log(!true);
+// console.log(!false);
+// console.log(!!false);
+
+// // NOT with numbers and strings
+
+// console.log(!0);
+// console.log(!1);
+// console.log(!!-200);
+// console.log(!'');
+// console.log(!'string');
+// console.log(!undefined);
+
+// // OR AND with numbers
+
+// console.log(25 || 36);
+// console.log(25 && 36);
+// console.log(0 || 36);
+// console.log(0 && 36);
+
+// // OR AND with strings
+
+// console.log('hello' || 'world');
+// console.log('hello' && 'world');
+// console.log('' || 'world');
+// console.log('' && 'world');
+
+// let a = 0;
+
+// a || console.log('a is falsy'); // if false it will run
+
+// a && console.log('a is not 0'); // if true will run
+
+// let b = 1 + 2;
+// let c = 1 + 2 * 3;
+// let d = (1 + 2) * 3;
+
+// console.log(b);
+// console.log(c);
+// console.log(d);
+
+// let e = 9 % 7;
+// console.log(e);
+
+// let f = 2 ** 3;
+
+// console.log(f);
+
+// let g = 7;
+// let h = 3;
+
+// g++;
+// h--;
+
+// console.log(g);
+// console.log(h);
+
+// let i = 27;
+
+// console.log(++i);
+// console.log(i++);
+
+// let j = 2;
+
+// let result = ++j * ++j; // 3 * 4
+// result = j++ * ++j; // 4 * 6
+
+// console.log(result);
+
+// let x = 1;
+
+// --x || console.log('LOGGED');
+
+// console.log(1 < 2);
+// console.log(1 > 2);
+// console.log(1 <= 2);
+// console.log(1 >= 2);
+// console.log(1 <= 1);
+// console.log(1 >= 1);
+
+// console.log('1' === 1);
+// console.log(NaN == NaN);
+// let m = 'A';
+// m++;
+// console.log(isNaN(m));
+// console.log(true !== 1);
+// console.log(true != 1);
+
+let n = 10;
+
+n * 3;
+
+if (n > 15) {
+  console.log('n is greater than 5');
+} else { console.log('n is not greater than 15'); }
+
+let o = 7;
+
+if (10 > o) {
+  console.log('YES o');
+} else { console.log('NO o'); }
